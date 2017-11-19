082a79d1449872a5ac4c1376bce53c32ea0b0dfe
Baxter Eaves <baxyjr@gmail.com>
2017-11-19 16:51:37 -0600

    Gauss score_accum benchmarks

---
+++ benches/bench_gauss.rs
@@ -0,0 +1,46 @@
+#![feature(test)]
+extern crate test;
+extern crate rand;
+extern crate braid;
+
+use test::Bencher;
+use rand::{Rng, XorShiftRng};
+use braid::cc::container::DataContainer;
+use braid::dist::Gaussian;
+use braid::dist::traits::{RandomVariate, AccumScore};
+
+
+#[bench]
+fn gauss_accum_score_serial(b: &mut Bencher) {
+    let n = 100_000;
+
+    let mut rng = XorShiftRng::new_unseeded();
+
+    let xs = Gaussian::new(-3.0, 1.0).sample(n, &mut rng);
+    let data = DataContainer::new(xs);
+    let gauss = Gaussian::new(0.0, 1.0);
+
+    let mut scores: Vec<f64> = vec![0.0; n];
+
+    b.iter(|| {
+        gauss.accum_score(&mut scores, &data.data, &data.present);
+    });
+}
+
+
+#[bench]
+fn gauss_accum_score_parallel(b: &mut Bencher) {
+    let n = 100_000;
+
+    let mut rng = XorShiftRng::new_unseeded();
+
+    let xs = Gaussian::new(-3.0, 1.0).sample(n, &mut rng);
+    let data = DataContainer::new(xs);
+    let gauss = Gaussian::new(0.0, 1.0);
+
+    let mut scores: Vec<f64> = vec![0.0; n];
+
+    b.iter(|| {
+        gauss.accum_score_par(&mut scores, &data.data, &data.present);
+    });
+}
