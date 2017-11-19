38e65f93a8479204e142b6d350fb19a29459d26b
Baxter Eaves <baxyjr@gmail.com>
2017-11-18 20:22:11 -0600

    Added one-column view bench

---
+++ benches/bench_view.rs
@@ -0,0 +1,46 @@
+#![feature(test)]
+
+extern crate braid;
+extern crate rand;
+extern crate test;
+
+use std::collections::BTreeMap;
+use rand::{Rng, XorShiftRng};
+use test::Bencher;
+
+use braid::cc::{Column, Feature, View, DataContainer};
+use braid::dist::Gaussian;
+use braid::cc::view::RowAssignAlg;
+use braid::dist::traits::RandomVariate;
+use braid::dist::prior::NormalInverseGamma;
+
+
+type GaussCol = Column<f64, Gaussian, NormalInverseGamma>;
+
+
+fn gendata_gauss(id: usize, n: usize, mut rng: &mut Rng) -> GaussCol {
+    let mut xs = Gaussian::new(-3.0, 1.0).sample(n, &mut rng);
+    let mut ys = Gaussian::new(3.0, 1.0).sample(n, &mut rng);
+
+    xs.append(&mut ys); 
+
+    let data = DataContainer::new(xs);
+    let prior = NormalInverseGamma::new(0.0, 1.0, 1.0, 1.0);
+
+    Column::new(id, data, prior)
+}
+
+
+#[bench]
+fn run_100row_01col_benchmark(b: &mut Bencher) {
+    let mut rng = XorShiftRng::new_unseeded();
+
+    let mut ftrs: BTreeMap<usize, Box<Feature>> = BTreeMap::new();
+    ftrs.insert(0, Box::new(gendata_gauss(0, 100, &mut rng)));
+
+    let mut view = View::new(ftrs, 1.0, &mut rng);
+
+    b.iter(|| {
+        test::black_box(view.update(1, RowAssignAlg::FiniteCpu, &mut rng));
+    });
+}
