56704804de82107e7124842dd5bcdd2ec3069816
Baxter Eaves <baxyjr@gmail.com>
2017-11-20 15:30:21 -0600

    tests for categorical suffstats

--- src/dist/categorical.rs
+++ src/dist/categorical.rs
@@ -73,9 +73,17 @@ impl<T> SufficientStatistic<T> for CategoricalSuffStats<T>
     }
 
     fn unobserve(&mut self, x: &T) {
+        if self.n == 0 {
+            panic!("No data to unobserve");
+        }
+
         let ix = (*x).clone().into();
         self.n -= 1;
         self.counts[ix] -= 1;
+
+        if self.counts[ix] < 0 {
+            panic!("Unobserved value ({}) that was never observed", ix);
+        }
    }
 }
 
@@ -146,7 +154,7 @@ mod tests {
 
 
     #[test]
-    fn categorical_new() {
+    fn new() {
         let ctgrl: Categorical<u8> = Categorical::new(vec![0.0, 0.1, 0.2]);
 
         assert_relative_eq!(ctgrl.log_weights[0], -1.20194285, epsilon = TOL);
@@ -155,7 +163,7 @@ mod tests {
     }
 
     #[test]
-    fn categorical_flat() {
+    fn flat() {
         let ctgrl: Categorical<u8> = Categorical::flat(3);
 
         assert_eq!(ctgrl.log_weights.len(), 3);
@@ -164,5 +172,57 @@ mod tests {
                             epsilon = TOL);
         assert_relative_eq!(ctgrl.log_weights[1], ctgrl.log_weights[2],
                             epsilon = TOL);
+    }
+
+    #[test]
+    fn suffstat_new() {
+        let sf: CategoricalSuffStats<u8> = CategoricalSuffStats::new(3);
+        assert!(sf.counts.iter().all(|&x| x == 0));
+    }
+
+    #[test]
+    fn suffstat_observe() {
+        let mut sf: CategoricalSuffStats<u8> = CategoricalSuffStats::new(3);
+        sf.observe(&0);
+        sf.observe(&1);
+        sf.observe(&2);
+        sf.observe(&0);
+        sf.observe(&2);
+        sf.observe(&0);
+
+        assert_eq!(sf.counts[0], 3);
+        assert_eq!(sf.counts[1], 1);
+        assert_eq!(sf.counts[2], 2);
+    }
+
+    #[test]
+    fn suffstat_unobserve() {
+        let mut sf: CategoricalSuffStats<u8> = CategoricalSuffStats{
+            n       : 6,
+            counts  : vec![3, 2, 1],
+            _phantom: PhantomData
+        };
+        sf.unobserve(&0);
+        sf.unobserve(&1);
+        sf.unobserve(&2);
+
+        assert_eq!(sf.counts[0], 2);
+        assert_eq!(sf.counts[1], 1);
+        assert_eq!(sf.counts[2], 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn unobserve_empty_panics() {
+        let mut sf: CategoricalSuffStats<u8> = CategoricalSuffStats::new(3);
+        sf.unobserve(&0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn unobserve_missing_count_panics() {
+        let mut sf: CategoricalSuffStats<u8> = CategoricalSuffStats::new(3);
+        sf.observe(&1);
+        sf.unobserve(&0);
     }
 }
