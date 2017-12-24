c47a8186bd4e70a6bda0e6ec8aa33ddae0dc6680
Baxter Eaves <baxyjr@gmail.com>
2017-12-23 22:10:02 -0600

    categroical sqlite data loader works

--- src/data/sqlite.rs
+++ src/data/sqlite.rs
@@ -57,10 +57,11 @@ fn sel_continuous_data(col: &str, table: &str, conn: &Connection)
 fn sel_categorical_data(col: &str, table: &str, conn: &Connection)
     -> DataContainer<u8>
 {
+    // FIXME: Dangerous!!!
     let query = format!("SELECT {} from {} ORDER BY id ASC;", col, table);
     let mut stmnt = conn.prepare(query.as_str()).unwrap();
     let data: Vec<u8> = stmnt
-        .query_map(&[&col, &table], |r| r.get(0))
+        .query_map(&[], |r| r.get(0))
         .unwrap()
         .map(|val| val.unwrap())
         .collect();
@@ -73,7 +74,7 @@ fn sel_categorical_data(col: &str, table: &str, conn: &Connection)
 mod tests {
     use super::*;
 
-    fn single_gauss_column_no_missing() -> Connection {
+    fn single_real_column_no_missing() -> Connection {
         let conn = Connection::open_in_memory().unwrap();
         conn.execute("CREATE TABLE data (
                       id  INTEGER PRIMARY KEY,
@@ -86,9 +87,22 @@ mod tests {
         conn
     }
 
+    fn single_int_column_no_missing() -> Connection {
+        let conn = Connection::open_in_memory().unwrap();
+        conn.execute("CREATE TABLE data (
+                      id  INTEGER PRIMARY KEY,
+                      x   INTEGER
+                      )", &[]).unwrap();
+        conn.execute("INSERT INTO data (id, x) VALUES (0, 1)", &[]).unwrap();
+        conn.execute("INSERT INTO data (id, x) VALUES (1, 2)", &[]).unwrap();
+        conn.execute("INSERT INTO data (id, x) VALUES (2, 3)", &[]).unwrap();
+        conn.execute("INSERT INTO data (id, x) VALUES (3, 4)", &[]).unwrap();
+        conn
+    }
+
     #[test]
     fn read_continuous_data_with_no_missing() {
-        let conn = single_gauss_column_no_missing();
+        let conn = single_real_column_no_missing();
 
         let data = sel_continuous_data(&"x", &"data", &conn);
 
@@ -101,7 +115,7 @@ mod tests {
 
     #[test]
     fn continuous_data_should_be_ordered_by_id() {
-        let conn = single_gauss_column_no_missing();
+        let conn = single_real_column_no_missing();
 
         // Add data out of order (by id)
         conn.execute("INSERT INTO data (id, x) VALUES (5, 5.5)", &[]).unwrap();
@@ -116,5 +130,18 @@ mod tests {
         assert_relative_eq!(data[3], 4.5, epsilon=10E-10);
         assert_relative_eq!(data[4], 4.4, epsilon=10E-10);
         assert_relative_eq!(data[5], 5.5, epsilon=10E-10);
+    }
+
+    #[test]
+    fn read_cateegorical_data_with_no_missing() {
+        let conn = single_int_column_no_missing();
+
+        let data = sel_categorical_data(&"x", &"data", &conn);
+
+        assert_eq!(data.len(), 4);
+        assert_eq!(data[0], 1);
+        assert_eq!(data[1], 2);
+        assert_eq!(data[2], 3);
+        assert_eq!(data[3], 4);
     }
 }
