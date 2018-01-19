21ce2b3d96e5dfabb77e2ceb1782c1550d4eb110
Baxter Eaves <baxyjr@gmail.com>
2018-01-18 19:48:08 -0600

    Added gamma distribution

---
+++ src/braid/dist/gamma.rs
@@ -0,0 +1,104 @@
+extern crate serde;
+extern crate rand;
+
+use std::f64;
+
+use self::rand::Rng;
+use self::rand::distributions::IndependentSample;
+
+use special::gammaln;
+use dist::traits::Distribution;
+use dist::traits::RandomVariate;
+use dist::traits::Moments;
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Gamma {
+    pub shape: f64,
+    pub rate: f64,
+}
+
+
+impl Gamma {
+    pub fn new(shape: f64, rate: f64) -> Gamma {
+        Gamma { shape: shape, rate: rate }
+    }
+}
+
+
+impl RandomVariate<f64> for Gamma {
+    fn draw(&self, mut rng: &mut Rng) -> f64 {
+        // The rand Gamma is parameterized by shape instead of rate
+        let g = rand::distributions::Gamma::new(self.shape, 1.0/self.rate);
+        g.ind_sample(&mut rng)
+    }
+
+    fn sample(&self, n: usize, mut rng: &mut Rng) -> Vec<f64> {
+        let g = rand::distributions::Gamma::new(self.shape, self.rate);
+        (0..n).map(|_| g.ind_sample(&mut rng)).collect()
+    }
+}
+
+
+impl Distribution<f64> for Gamma {
+    fn log_normalizer(&self) -> f64 {
+        0.0
+    }
+
+    fn unnormed_loglike(&self, x: &f64) -> f64 {
+        let a = self.shape;
+        let b = self.rate;
+        a * b.ln() - gammaln(a) + (a - 1.0) * x.ln() - (b * x)
+    }
+}
+
+
+impl Moments<f64, f64> for Gamma {
+    fn mean(&self) -> f64 {
+        self.shape / self.rate
+    }
+
+    fn var(&self) -> f64 {
+        self.shape / (self.rate * self.rate)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    extern crate serde_yaml;
+    use super::*;
+
+    const TOL: f64 = 1E-8;
+
+    #[test]
+    fn gamma_new() {
+        let g = Gamma::new(1.2, 3.4);
+        assert_relative_eq!(g.shape, 1.2, epsilon=TOL);
+        assert_relative_eq!(g.rate, 3.4, epsilon=TOL);
+    }
+
+    #[test]
+    fn gamma_mean() {
+        let g = Gamma::new(1.2, 3.4);
+        assert_relative_eq!(g.mean(), 1.2/3.4, epsilon=TOL);
+    }
+
+    #[test]
+    fn gamma_var() {
+        let g = Gamma::new(1.2, 3.4);
+        assert_relative_eq!(g.var(), 1.2/(3.4 * 3.4), epsilon=TOL);
+    }
+
+    #[test]
+    fn gamma_loglike_1() {
+        let g = Gamma::new(1.0, 1.0);
+        assert_relative_eq!(g.loglike(&1.5), -1.5, epsilon=TOL);
+    }
+
+    #[test]
+    fn gamma_loglike_2() {
+        let g = Gamma::new(1.2, 3.4);
+        assert_relative_eq!(g.loglike(&1.5), -3.465002370428512, epsilon=TOL);
+    }
+}
--- src/braid/dist/mod.rs
+++ src/braid/dist/mod.rs
@@ -3,6 +3,7 @@ pub mod bernoulli;
 pub mod categorical;
 pub mod dirichlet;
 pub mod gaussian;
+pub mod gamma;
 pub mod prior;
 pub mod mixture;
 
@@ -12,4 +13,5 @@ pub use dist::categorical::Categorical;
 pub use dist::dirichlet::Dirichlet;
 pub use dist::dirichlet::SymmetricDirichlet;
 pub use dist::gaussian::Gaussian;
+pub use dist::gamma::Gamma;
 pub use dist::mixture::MixtureModel;
