e34d3e836798e62e27d3171b35c2f7a96081757a
Baxter Eaves <baxyjr@gmail.com>
2018-03-25 22:52:37 -0500

    fix vew benches

--- benches/bench_view.rs
+++ benches/bench_view.rs
@@ -1,11 +1,13 @@
 #![feature(test)]
 
+#[macro_use]
+extern crate criterion;
+
 extern crate braid;
 extern crate rand;
-extern crate test;
 
 use rand::{Rng, XorShiftRng};
-use test::Bencher;
+use criterion::Criterion;
 
 use braid::cc::{ColModel, Column, DataContainer, View};
 use braid::dist::Gaussian;
@@ -13,28 +15,30 @@ use braid::cc::view::RowAssignAlg;
 use braid::dist::traits::RandomVariate;
 use braid::dist::prior::NormalInverseGamma;
 
-fn gendata_gauss(id: usize, n: usize, mut rng: &mut Rng) -> ColModel {
-    let mut xs = Gaussian::new(-3.0, 1.0).sample(n, &mut rng);
-    let mut ys = Gaussian::new(3.0, 1.0).sample(n, &mut rng);
-
-    xs.append(&mut ys);
-
+fn gen_gauss_col(id: usize, n: usize, mut rng: &mut Rng) -> ColModel {
+    let xs = Gaussian::new(-3.0, 1.0).sample(n, &mut rng);
     let data = DataContainer::new(xs);
-    let prior = NormalInverseGamma::new(0.0, 1.0, 1.0, 1.0);
+    let prior = NormalInverseGamma::from_data(&data.data, &mut rng);
 
     ColModel::Continuous(Column::new(id, data, prior))
 }
 
-#[bench]
-fn run_100row_01col_benchmark(b: &mut Bencher) {
-    let mut rng = XorShiftRng::new_unseeded();
-
-    let mut ftrs: Vec<ColModel> = vec![];
-    ftrs.insert(0, gendata_gauss(0, 100, &mut rng));
-
-    let mut view = View::new(ftrs, 1.0, &mut rng);
-
-    b.iter(|| {
-        test::black_box(view.update(1, RowAssignAlg::FiniteCpu, &mut rng));
-    });
+fn gen_gauss_view(nrows: usize, ncols: usize, mut rng: &mut Rng) -> View {
+   let ftrs: Vec<ColModel> = (0..ncols)
+       .map(|id| gen_gauss_col(id, nrows, &mut rng))
+       .collect();
+   View::new(ftrs, 1.0, &mut rng)
 }
+
+fn bench_gauss_view_reassign_finite_cpu(c: &mut Criterion) {
+    c.bench_function_over_inputs("gauss view finite CPU reassign", |b, &&n| {
+        b.iter(|| {
+            let mut rng = XorShiftRng::new_unseeded();
+            let mut view = gen_gauss_view(n, 5, &mut rng);
+            view.reassign(RowAssignAlg::FiniteCpu, &mut rng);
+        })
+    }, &[10, 100, 1000, 10000]);
+}
+
+criterion_group!(benches, bench_gauss_view_reassign_finite_cpu);
+criterion_main!(benches);
