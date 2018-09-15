9dfac100698dcffe795577ccf99dae4a740a9844
Baxter Eaves <baxyjr@gmail.com>
2018-09-15 12:25:56 -0500

    PIT stablization

--- src/misc/pit.rs
+++ src/misc/pit.rs
@@ -7,7 +7,7 @@ use self::rv::traits::{Cdf, Rv};
 fn pit_quad_lower_part(a: f64, b: f64, f: f64) -> f64 {
     let q =
         0.5 * f * (a.powi(2) - b.powi(2)) + 1.0 / 3.0 * (b.powi(3) - a.powi(3));
-    assert!(q > 0.0);
+    assert!(q >= 0.0);
     q
 }
 
@@ -15,31 +15,21 @@ fn pit_quad_lower_part(a: f64, b: f64, f: f64) -> f64 {
 fn pit_quad_upper_part(a: f64, b: f64, f: f64) -> f64 {
     let q =
         0.5 * f * (b.powi(2) - a.powi(2)) + 1.0 / 3.0 * (a.powi(3) - b.powi(3));
-    assert!(q > 0.0);
-    q
-}
-
-#[inline]
-fn pit_quad_lower_part_area(a: f64, b: f64, f: f64) -> f64 {
-    let q = f * (a - b) + 0.5 * (b.powi(2) - a.powi(2));
-    assert!(q > 0.0);
-    q
-}
-
-#[inline]
-fn pit_quad_upper_part_area(a: f64, b: f64, f: f64) -> f64 {
-    let q = f * (b - a) + 0.5 * (a.powi(2) - b.powi(2));
-    assert!(q > 0.0);
+    assert!(q >= 0.0);
     q
 }
 
 fn pit_area_quad_prtl(a: f64, b: f64, f: f64) -> f64 {
-    if f < a {
-        pit_quad_lower_part_area(a, b, f)
-    } else if f > b {
-        pit_quad_upper_part_area(a, b, f)
+    if f <= a {
+        // pit_quad_lower_part_area(a, b, f)
+        f * (a - b) + 0.5 * (b*b - a*a)
+    } else if f >= b {
+        // pit_quad_upper_part_area(a, b, f)
+        f * (b - a) + 0.5 * (a*a - b*b)
     } else {
-        pit_quad_lower_part_area(a, f, f) + pit_quad_upper_part_area(f, b, f)
+        // Can exploit the f line's intersecting the uniform CDF line to
+        // simplify the math. It becomes the area of two triangles.
+        0.5 * (f - a).powi(2) + 0.5 * (b - f).powi(2)
     }
 }
 
@@ -49,7 +39,7 @@ fn pit_quad_prtl(a: f64, b: f64, f: f64) -> f64 {
     } else if f > b {
         pit_quad_upper_part(a, b, f)
     } else {
-        pit_quad_lower_part(a, f, f) + pit_quad_upper_part(f, b, f)
+        pit_quad_upper_part(a, f, f) + pit_quad_lower_part(f, b, f)
     }
 }
 
@@ -91,6 +81,7 @@ where
         acc + q
     });
 
+    a = 0.0;
     let quad = ps.iter().enumerate().fold(0.0, |acc, (ix, &b)| {
         let f = (ix as f64) / nf;
         let q = pit_quad_prtl(a, b, f);
@@ -99,7 +90,51 @@ where
     });
 
     let centroid = quad / area;
-    let error = 2.0 * quad; // should be in [0, 1]
+    let error = area; // should be in [0, 0.5]
 
     (error, centroid)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use self::rv::dist::Gaussian;
+
+    #[test]
+    fn pit_for_samples_from_target_should_have_low_error() {
+        let mut rng = rand::thread_rng();
+        let g = Gaussian::standard();
+        let xs: Vec<f64> = g.sample(1000, &mut rng);
+        let mixture = Mixture::new(vec![1.0], vec![g]).unwrap();
+        let (error, centroid) = pit(&xs, &mixture);
+        assert!(error < 0.05);
+        assert!((centroid - 0.5).abs() < 0.1);
+    }
+
+    #[test]
+    fn pit_for_samples_from_narrow_should_have_high_error() {
+        let mut rng = rand::thread_rng();
+        let g_gen = Gaussian::standard();
+        let g_target = Gaussian::new(0.0, 0.1).unwrap();
+        let xs: Vec<f64> = g_gen.sample(100, &mut rng);
+        let mixture = Mixture::new(vec![1.0], vec![g_target]).unwrap();
+        let (error, centroid) = pit(&xs, &mixture);
+        assert!(error > 0.2);
+        // The means are the same, so the error centroid should be around 0.5
+        assert!((centroid - 0.5).abs() < 0.07);
+    }
+
+    #[test]
+    fn pit_for_samples_from_wide_should_have_high_error() {
+        let mut rng = rand::thread_rng();
+        let g_gen = Gaussian::standard();
+        let g_target = Gaussian::new(1.5, 1.0).unwrap();
+        let xs: Vec<f64> = g_gen.sample(100, &mut rng);
+        let mixture = Mixture::new(vec![1.0], vec![g_target]).unwrap();
+        let (error, centroid) = pit(&xs, &mixture);
+        assert!(error > 0.25);
+        // Since the target is shifted right, the error centroid should be to
+        // the left.
+        assert!(centroid < 0.45);
+    }
 }
