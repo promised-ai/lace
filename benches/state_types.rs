fb191037a459985c7f96b79d9b9194f72f40396c
Baxter Eaves <bax@redpoll.ai>
2019-06-20 20:09:16 -0500

    Use macro for state_type benchmarks

--- benches/state_types.rs
+++ benches/state_types.rs
@@ -8,79 +8,55 @@ use braid::cc::config::StateUpdateConfig;
 use braid::cc::StateBuilder;
 use braid_codebook::codebook::ColType;
 
-fn bench_labeler_state(c: &mut Criterion) {
-    c.bench_function("20-by-2 (1 views, 5 cats) all-labeler state", |b| {
-        let labeler_config = ColType::Labeler {
-            pr_h: None,
-            pr_k: None,
-            pr_world: None,
-        };
-        let builder = StateBuilder::new()
-            .with_rows(20)
-            .add_column_configs(2, labeler_config)
-            .with_views(1)
-            .with_cats(5)
-            .with_seed(1337);
-        let mut rng = Xoshiro256Plus::seed_from_u64(1337);
-        b.iter_batched(
-            || builder.clone().build().unwrap(),
-            |mut state| {
-                let config = StateUpdateConfig::default();
-                black_box(state.update(config, &mut rng));
-            },
-            BatchSize::LargeInput,
-        )
-    });
+macro_rules! state_type_bench {
+    ($id: expr, $fn: ident, $config: expr) => {
+        fn $fn(c: &mut Criterion) {
+            c.bench_function($id, |b| {
+                let builder = StateBuilder::new()
+                    .with_rows(20)
+                    .add_column_configs(2, $config)
+                    .with_views(1)
+                    .with_cats(5)
+                    .with_seed(1337);
+                let mut rng = Xoshiro256Plus::seed_from_u64(1337);
+                b.iter_batched(
+                    || builder.clone().build().unwrap(),
+                    |mut state| {
+                        let config = StateUpdateConfig::default();
+                        black_box(state.update(config, &mut rng));
+                    },
+                    BatchSize::LargeInput,
+                )
+            });
+        }
+    }
 }
 
-fn bench_categorical_state(c: &mut Criterion) {
-    c.bench_function(
-        "20-by-2 (1 views, 5 cats) all-Categorical(2) state",
-        |b| {
-            let labeler_config = ColType::Categorical {
-                k: 2,
-                hyper: None,
-                value_map: None,
-            };
-            let builder = StateBuilder::new()
-                .with_rows(20)
-                .add_column_configs(2, labeler_config)
-                .with_views(1)
-                .with_cats(5)
-                .with_seed(1337);
-            let mut rng = Xoshiro256Plus::seed_from_u64(1337);
-            b.iter_batched(
-                || builder.clone().build().unwrap(),
-                |mut state| {
-                    let config = StateUpdateConfig::default();
-                    black_box(state.update(config, &mut rng));
-                },
-                BatchSize::LargeInput,
-            )
-        },
-    );
-}
+state_type_bench!(
+    "20-by-2 (1 views, 5 cats) all-labeler state",
+    bench_labeler_state,
+    ColType::Labeler {
+        pr_h: None,
+        pr_k: None,
+        pr_world: None,
+    }
+);
 
-fn bench_gaussian_state(c: &mut Criterion) {
-    c.bench_function("20-by-2 (1 views, 5 cats) all-Gaussian state", |b| {
-        let labeler_config = ColType::Continuous { hyper: None };
-        let builder = StateBuilder::new()
-            .with_rows(20)
-            .add_column_configs(2, labeler_config)
-            .with_views(1)
-            .with_cats(5)
-            .with_seed(1337);
-        let mut rng = Xoshiro256Plus::seed_from_u64(1337);
-        b.iter_batched(
-            || builder.clone().build().unwrap(),
-            |mut state| {
-                let config = StateUpdateConfig::default();
-                black_box(state.update(config, &mut rng));
-            },
-            BatchSize::LargeInput,
-        )
-    });
-}
+state_type_bench!(
+    "20-by-2 (1 views, 5 cats) all-categorical(2) state",
+    bench_categorical_state,
+    ColType::Categorical {
+        k: 2,
+        hyper: None,
+        value_map: None,
+    }
+);
+
+state_type_bench!(
+    "20-by-2 (1 views, 5 cats) all-gaussian state",
+    bench_gaussian_state,
+    ColType::Continuous { hyper: None }
+);
 
 criterion_group!(
     state_type_benches,
