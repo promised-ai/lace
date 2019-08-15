fa07148be43e88b461891688d378f3a7724a7671
Baxter Eaves <bax@redpoll.ai>
2019-08-15 12:55:28 -0500

    Add docs to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,118 @@
 //! A fast, extensible probabilistic cross-categorization engine.
+//!
+//! # Example
+//!
+//! Analyze structure in the pre-run `animals` example. Each row is an animal
+//! and each column is a feature of that animal. The feature is present if the
+//! cell value is 1 and is absent if the value is 0.
+//!
+//! First, we create an oracle and import some `enum`s that allow us to call
+//! out some of the row and column indices in plain English.
+//!
+//! ```rust
+//! use braid::examples::Example;
+//! use braid::examples::animals::{Row, Column};
+//!
+//! let oracle = Example::Animals.oracle().unwrap();
+//! ```
+//!
+//! Let's ask about the statistical dependence between whether something swims
+//! and is fast or has flippers. We expect the something swimming is more
+//! indicative of whether it swims than whether something is fast, therefore we
+//! expect the dependence between swims and flippers to be higher.
+//!
+//! ```rust
+//! # use braid::examples::Example;
+//! # use braid::examples::animals::{Row, Column};
+//! # let oracle = Example::Animals.oracle().unwrap();
+//! let depprob_fast = oracle.depprob(
+//!     Column::Swims.into(),
+//!     Column::Fast.into(),
+//! );
+//!
+//! let depprob_flippers = oracle.depprob(
+//!     Column::Swims.into(),
+//!     Column::Flippers.into(),
+//! );
+//!
+//! assert!(depprob_flippers > depprob_fast);
+//! ```
+//!
+//! We have the same expectation of mutual information. Mutual information
+//! requires more input from the user. We need to know what type of mutual
+//! information, how many samples to take if we need to estimate the mutual
+//! information, and a random number generator for the Monte Carlo integrator.
+//!
+//! ```rust
+//! # use braid::examples::Example;
+//! # use braid::examples::animals::{Row, Column};
+//! # let oracle = Example::Animals.oracle().unwrap();
+//! use braid::interface::MiType;
+//!
+//! let mut rng = rand::thread_rng();
+//!
+//! let mi_fast = oracle.mi(
+//!     Column::Swims.into(),
+//!     Column::Fast.into(),
+//!     1000,
+//!     MiType::Iqr,
+//!     &mut rng,
+//! );
+//!
+//! let mi_flippers = oracle.mi(
+//!     Column::Swims.into(),
+//!     Column::Flippers.into(),
+//!     1000,
+//!     MiType::Iqr,
+//!     &mut rng,
+//! );
+//!
+//! assert!(mi_flippers > mi_fast);
+//! ```
+//!
+//! We can likewise ask about the similarity between rows -- in this case,
+//! animals.
+//!
+//! ```
+//! # use braid::examples::Example;
+//! # use braid::examples::animals::Row;
+//! # let oracle = Example::Animals.oracle().unwrap();
+//! let rowsim_wolf = oracle.rowsim(
+//!     Row::Wolf.into(),
+//!     Row::Chihuahua.into(),
+//!     None
+//! );
+//!
+//! let rowsim_rat = oracle.rowsim(
+//!     Row::Rat.into(),
+//!     Row::Chihuahua.into(),
+//!     None
+//! );
+//!
+//! assert!(rowsim_rat > rowsim_wolf);
+//! ```
+//!
+//! And we can add context to similarity.
+//!
+//! ```
+//! # use braid::examples::Example;
+//! # use braid::examples::animals::{Column, Row};
+//! # let oracle = Example::Animals.oracle().unwrap();
+//! let context = vec![Column::Swims.into()];
+//! let rowsim_otter = oracle.rowsim(
+//!     Row::Beaver.into(),
+//!     Row::Otter.into(),
+//!     Some(&context),
+//! );
+//!
+//! let rowsim_dolphin = oracle.rowsim(
+//!     Row::Beaver.into(),
+//!     Row::Dolphin.into(),
+//!     Some(&context),
+//! );
+//!
+//! assert!(rowsim_otter > rowsim_dolphin);
+//! ```
 pub mod cc;
 pub mod data;
 pub mod defaults;
