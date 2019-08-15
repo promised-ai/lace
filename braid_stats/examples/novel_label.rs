56958d46fbb8fa40da57fc2313264e403667cd3f
Baxter Eaves <bax@redpoll.ai>
2019-08-15 09:11:23 -0500

    Update novel_label example
    New example takes command line args reflecting the number of labels in
    the world and the output file name

--- braid_stats/examples/novel_label.rs
+++ braid_stats/examples/novel_label.rs
@@ -1,18 +1,30 @@
-// Observe the labeler state over time. In this experiment, the true label is
-// always 'false' and informant always labels 'false'. We then ask the labeler
-// how the informant would label 'true'.
+// Observe the labeler state over time as an informant labels perfectly
+// correctly. States are saved to a csv.
 //
-// The code outputs to a csv called 'novel-label.csv'.
+// Use:
+//
+//     cargo run --example novel_label <N_LABELS> <OUTPUT.CSV>
+//
+// Example: A world where there are two labels, but only one is observed.
+//
+//     cargo run --example novel_label 1 novel-label.csv
+//
+// Example: A world where there are four labels, but only three are observed.
+//
+//     cargo run --example novel_label 3 novel-label.csv
 //
 // If you'd like to plot with python, the following code will do the trick:
 //
 //     import pandas as pd
 //     import matplotlib.pyplot as plt
+//     import seaborn as sns
 //
 //     df = pd.read_csv("novel-label.csv");
-//     df.plot(figsize=(8, 4), lw=1, alpha=0.5)
-//     plt.savefig("plot.png", dpi=150)
+//     sns.lineplot(x="t", y="p", hue="measure", data=df)
 //
+//     plt.show()
+//
+use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufWriter;
@@ -23,44 +35,83 @@ use rv::traits::*;
 
 use braid_stats::labeler::*;
 
-fn p_label_novel_correctly(labeler: &Labeler) -> f64 {
-    let ft = Label::new(0, Some(1));
-    let tt = Label::new(1, Some(1));
+fn p_label_novel_correctly(labeler: &Labeler, n_labels: u8) -> f64 {
+    let ps: Vec<f64> = (0..n_labels + 1)
+        .map(|x| {
+            let label = Label::new(x, Some(n_labels));
+            labeler.f(&label)
+        })
+        .collect();
 
-    let pt = labeler.f(&tt);
-    let pf = labeler.f(&ft);
-
-    pt / (pf + pt)
+    ps.last().unwrap() / ps.iter().sum::<f64>()
 }
 
 fn main() {
-    let file = File::create("novel-label.csv").unwrap();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Please provide N_LABELS and OUTPUT.CSV args.");
+        std::process::exit(1)
+    }
+
+    let n_labels: u8 = args[1].parse().unwrap();
+    let fileout = &args[2];
+
+    let file = File::create(fileout).unwrap();
     let mut writer = BufWriter::new(file);
 
-    writer.write("p_k,p_h,p_w,p_correct\n".as_bytes()).unwrap();
+    writer.write("rep,measure,t,p\n".as_bytes()).unwrap();
 
-    let n = 1_000;
+    let n = 200;
+    let n_reps = 20;
 
     let mut rng = rand::thread_rng();
 
-    let prior = LabelerPrior::standard(2);
-    let mut stat = LabelerSuffStat::new();
-    let x = Label::new(0, Some(0));
+    for rep in 0..n_reps {
+        let prior = LabelerPrior::uniform(n_labels + 1);
 
-    (1..=n).for_each(|_| {
-        stat.observe(&x);
-        let posterior = prior.posterior(&DataOrSuffStat::SuffStat(&stat));
-        let labeler = posterior.draw(&mut rng);
+        let mut stat = LabelerSuffStat::new();
 
-        let line = format!(
-            "{},{},{:?},{}\n",
-            labeler.p_k(),
-            labeler.p_h(),
-            labeler.p_world().point()[1],
-            p_label_novel_correctly(&labeler),
-        );
+        (1..=n).for_each(|t| {
+            let label = t % n_labels;
 
-        writer.write(line.as_bytes()).unwrap();
-    });
-    println!("");
+            let x = Label::new(label, Some(label));
+            stat.observe(&x);
+
+            let posterior = prior.posterior(&DataOrSuffStat::SuffStat(&stat));
+            let labeler = posterior.draw(&mut rng);
+
+            {
+                let line =
+                    format!("{},{},{},{}\n", rep, "p(k)", t, labeler.p_k());
+                writer.write(line.as_bytes()).unwrap();
+            }
+            {
+                let line =
+                    format!("{},{},{},{}\n", rep, "p(h)", t, labeler.p_h());
+                writer.write(line.as_bytes()).unwrap();
+            }
+            {
+                let line = format!(
+                    "{},{},{},{}\n",
+                    rep,
+                    "p(w=2)",
+                    t,
+                    labeler.p_world().point()[n_labels as usize],
+                );
+                writer.write(line.as_bytes()).unwrap();
+            }
+            {
+                let line = format!(
+                    "{},{},{},{}\n",
+                    rep,
+                    "p(l=2|w=2)",
+                    t,
+                    p_label_novel_correctly(&labeler, n_labels),
+                );
+
+                writer.write(line.as_bytes()).unwrap();
+            }
+        });
+    }
 }
