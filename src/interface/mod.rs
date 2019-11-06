067eb0dfb97235f45e9064df624fa1c0c7152c79
Baxter Eaves <bax@redpoll.ai>
2019-11-06 15:25:42 -0600

    Reorganize given and add converters

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,10 +1,8 @@
 //! User-interface objects for running and querying
 mod bencher;
 mod engine;
+mod given;
 mod oracle;
-
-use braid_stats::Datum;
-use serde::{Deserialize, Serialize};
 
 pub use engine::Engine;
 pub use engine::EngineBuilder;
@@ -21,52 +19,5 @@ pub use bencher::Bencher;
 pub use bencher::BencherResult;
 pub use bencher::BencherRig;
 
-/// Describes a the conditions (or not) on a conditional distribution
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, PartialOrd)]
-pub enum Given {
-    /// The conditions in `(column_id, value)` tuples. The tuple
-    /// `(12, Datum::Continuous(2.3))` indicates that we wish to condition on
-    /// the value of column 12 being 2.3.
-    #[serde(rename = "conditions")]
-    Conditions(Vec<(usize, Datum)>),
-    /// The absence of conditioning observations
-    #[serde(rename = "nothing")]
-    Nothing,
-}
-
-impl Given {
-    /// Determine whether there are no conditions
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # use braid_stats::Datum;
-    /// # use braid::Given;
-    /// let nothing_given = Given::Nothing;
-    ///
-    /// assert!(nothing_given.is_nothing());
-    ///
-    /// let something_given = Given::Conditions(vec![(0, Datum::Categorical(1))]);
-    ///
-    /// assert!(!something_given.is_nothing());
-    /// ```
-    pub fn is_nothing(&self) -> bool {
-        match self {
-            Given::Nothing => true,
-            _ => false,
-        }
-    }
-
-    pub fn is_conditions(&self) -> bool {
-        match self {
-            Given::Conditions(..) => true,
-            _ => false,
-        }
-    }
-}
-
-impl Default for Given {
-    fn default() -> Self {
-        Given::Nothing
-    }
-}
+pub use given::Given;
+pub use given::IntoGivenError;
