66bb4acbb3c9d2f83fe22be07c7318f1d1a7aed6
Sabree Blackmon <sabree@heavypackets.sh>
2020-01-29 10:00:03 -0600

    Update test bootstrap so it can be ran multiple times without fail

--- scripts/test-setup.sh
+++ scripts/test-setup.sh
@@ -4,12 +4,12 @@ set -e
 
 BRAID_TEST_DIR=braid-tests
 
-mkdir $BRAID_TEST_DIR
+mkdir -p $BRAID_TEST_DIR
 
 VALID_DIR_1=$BRAID_TEST_DIR/savedata.valid.1.braid
 
 # Generate a test save directory
-mkdir $VALID_DIR_1
+mkdir -p $VALID_DIR_1
 touch $VALID_DIR_1/0.state
 touch $VALID_DIR_1/1.state
 touch $VALID_DIR_1/2.state
@@ -19,7 +19,7 @@ touch $VALID_DIR_1/test.data
 NO_CODEBOOK_DIR=$BRAID_TEST_DIR/savedata.no.codebook.braid
 
 # Generate a test save directory
-mkdir $NO_CODEBOOK_DIR
+mkdir -p $NO_CODEBOOK_DIR
 touch $NO_CODEBOOK_DIR/0.state
 touch $NO_CODEBOOK_DIR/1.state
 touch $NO_CODEBOOK_DIR/test.data
@@ -27,7 +27,7 @@ touch $NO_CODEBOOK_DIR/test.data
 NO_DATA_DIR=$BRAID_TEST_DIR/savedata.no.data.braid
 
 # Generate a test save directory
-mkdir $NO_DATA_DIR
+mkdir -p $NO_DATA_DIR
 touch $NO_DATA_DIR/0.state
 touch $NO_DATA_DIR/1.state
 touch $NO_DATA_DIR/2.state
