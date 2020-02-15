affde77ef68e3ed412b7e58d6fed9079c3af86c3
Baxter Eaves <bax@redpoll.ai>
2020-02-14 20:17:30 -0600

    publish to cloudsmith in CI

--- publish.sh
+++ publish.sh
@@ -9,18 +9,39 @@ crate_version() {
     echo $version
 }
 
+check_published() {
+    crate_name=$1
+    version=$2
+
+    matches=`cloudsmith ls pkgs -k ${CLOUDSMITH_API_KEY} --query "name:${crate_name} version:${version}" redpoll/crates | grep -o "${crate_name}" -c`
+
+    if [ "${matches}" -eq 0 ]; then
+        echo "no"
+    elif [ "${matches}" -eq 1 ]; then
+        echo "yes"
+    else
+        echo "Package search should have returned 0 or 1, but returned ${matches}"
+        exit 1
+    fi
+}
+
 publish_crate() {
     crate_name=$1
     crate_path=$2
     version=`crate_version $crate_path`
 
-    echo ""
-    echo "==Publishing ${crate_name} v${version}=="
-    cd $crate_path
-    cargo package -q
-    cloudsmith push cargo \
-        redpoll/crates \
-        $DIR/target/package/${crate_name}-${version}.crate
+    if [ `check_published $crate_name $version` = "no" ]; then
+        echo "== Publishing ${crate_name} v${version} =="
+        cd $crate_path
+        cargo package -q
+        cloudsmith push cargo \
+            -k ${CLOUDSMITH_API_KEY} \
+            redpoll/crates \
+            $DIR/target/package/${crate_name}-${version}.crate
+    else
+        echo "-- ${crate_name} v${version} already published. Skipping. --"
+    fi
+
 }
 
 publish_subcrate() {
