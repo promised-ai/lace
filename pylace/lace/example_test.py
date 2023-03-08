fee6735eebae11e8ef93bd6bb25200606bd86d7f
Mike Schmidt <schmidmt@gmail.com>
2023-03-08 09:46:35 -0700

    ci: Added basic example test

---
+++ pylace/lace/example_test.py
@@ -0,0 +1,15 @@
+"""Check the basics of the example datasets."""
+from numpy.testing import assert_almost_equal
+
+from lace import examples
+
+
+def test_animals():
+    """Check the output of the animals example."""
+
+    engine = examples.Animals()
+
+    assert engine.shape == (50, 85)
+    swim_cat, swim_unc = engine.predict("swims")
+    assert swim_cat == 0
+    assert_almost_equal(swim_unc, 0.008287, 6)
