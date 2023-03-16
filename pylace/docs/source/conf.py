431630921104c84d08b755ea9f7777262875fbaa
Ken Swanson <ken.swanson@redpoll.ai>
2023-03-16 13:49:41 -0500

    `black` fixes

--- pylace/docs/source/conf.py
+++ pylace/docs/source/conf.py
@@ -6,28 +6,27 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Lace'
-copyright = '2023, Promised AI'
-author = 'Promised AI'
+project = "Lace"
+copyright = "2023, Promised AI"
+author = "Promised AI"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
-    ]
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+]
 
 autosummary_generate = True
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
