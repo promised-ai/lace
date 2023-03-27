dcaeaf15a53efac4f6affbcf1b09d81b5ae5621a
Ken Swanson <ken.swanson@redpoll.ai>
2023-03-27 12:50:59 -0500

    Fleshed out code for running python examples

--- .github/scripts/run_code_in_mdfile.py
+++ .github/scripts/run_code_in_mdfile.py
@@ -10,11 +10,17 @@ args = parser.parse_args()
 
 fh = open(args.file)
 
-result=subprocess.run(['codedown', args.language], stdin=fh, capture_output=True, text=True)
+result=subprocess.run(['codedown', args.language + '*'], stdin=fh, capture_output=True, text=True)
 
 code = result.stdout.strip()
 
 if len(code) == 0:
     exit(0)
 
-exit(1)
+if args.language == 'python':
+    code_result=subprocess.run('python3', input=code, text=True)
+    exit(code_result.returncode)
+elif args.language == 'rust':
+    exit(1)
+else:
+    exit(1)
