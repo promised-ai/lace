ae79c63cc567d63eea67adb04b6c5f955be6aa88
Ken Swanson <ken.swanson@redpoll.ai>
2023-04-04 19:16:00 -0500

    Added line numbers to the code output (to aid in debugging)

--- .github/scripts/run_code_in_mdfile.py
+++ .github/scripts/run_code_in_mdfile.py
@@ -15,13 +15,19 @@ result=subprocess.run(['codedown', args.language + '*'], stdin=fh, capture_outpu
 
 code = result.stdout.strip()
 
+def enumerate_string_lines(code):
+    i=1
+    print("```")
+    for line in code.split("\n"):
+        print(f"{i:>3} {line}")
+        i+=1
+    print("```")
+
 if len(code) == 0:
     exit(0)
 
 if args.language == 'python':
-    print(f"""```{code}
-```
-""")
+    enumerate_string_lines(code)
     code_result=subprocess.run('python3', input=code, text=True)
     exit(code_result.returncode)
 elif args.language == 'rust':
@@ -34,10 +40,7 @@ fn main() {{
 }}
 """
 
-    print(f"""```
-{rust_script_contents}
-```
-""")
+    enumerate_string_lines(rust_script_contents)
 
     rust_script_file=tempfile.NamedTemporaryFile(mode='w', dir='./lace')
     rust_script_file.write(rust_script_contents)
