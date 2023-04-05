5a0e40bddc69b8de6330cbdd4a1719b8ae27cd8a
Ken Swanson <ken.swanson@redpoll.ai>
2023-04-04 19:02:53 -0500

    Fix to get rid of `unused doc comment` warning when testing `rust`

--- .github/scripts/run_code_in_mdfile.py
+++ .github/scripts/run_code_in_mdfile.py
@@ -19,26 +19,28 @@ if len(code) == 0:
     exit(0)
 
 if args.language == 'python':
-    print(f"""```
-{code}
+    print(f"""```{code}
 ```
 """)
     code_result=subprocess.run('python3', input=code, text=True)
     exit(code_result.returncode)
 elif args.language == 'rust':
-    rust_script_file=tempfile.NamedTemporaryFile(mode='w', dir='./lace')
-    rust_script_file.write("""
-//! ```cargo
+    rust_script_contents=f"""//! ```cargo
 //! [dependencies]
-//! lace = { path = ".", version="0.1.0" }
+//! lace = {{ path = ".", version="0.1.0" }}
 //! ```
-
-""")
-    print(f"""```
+fn main() {{
 {code}
+}}
+"""
+
+    print(f"""```
+{rust_script_contents}
 ```
 """)
-    rust_script_file.write(code)
+
+    rust_script_file=tempfile.NamedTemporaryFile(mode='w', dir='./lace')
+    rust_script_file.write(rust_script_contents)
     rust_script_file.flush()
 
     code_result=subprocess.run(['rust-script', rust_script_file.name])
