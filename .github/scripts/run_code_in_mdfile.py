03f152503abfee1f2bb31b8e6d3bee0ad0854b96
Ken Swanson <ken.swanson@redpoll.ai>
2023-04-05 14:23:02 -0500

    Pulled functionality into a function

--- .github/scripts/run_code_in_mdfile.py
+++ .github/scripts/run_code_in_mdfile.py
@@ -9,12 +9,6 @@ parser.add_argument('language')
 parser.add_argument('file')
 args = parser.parse_args()
 
-fh = open(args.file)
-
-result=subprocess.run(['codedown', args.language + '*'], stdin=fh, capture_output=True, text=True)
-
-code = result.stdout.strip()
-
 def enumerate_string_lines(code):
     i=1
     print("```")
@@ -23,30 +17,40 @@ def enumerate_string_lines(code):
         i+=1
     print("```")
 
-if len(code) == 0:
-    exit(0)
+def process_file(file, language):
+    fh = open(file)
 
-if args.language == 'python':
-    enumerate_string_lines(code)
-    code_result=subprocess.run('python3', input=code, text=True)
-    exit(code_result.returncode)
-elif args.language == 'rust':
-    rust_script_contents=f"""//! ```cargo
-//! [dependencies]
-//! lace = {{ path = ".", version="0.1.0" }}
-//! ```
-fn main() {{
-{code}
-}}
-"""
+    result=subprocess.run(['codedown', language + '*'], stdin=fh, capture_output=True, text=True)
 
-    enumerate_string_lines(rust_script_contents)
+    code = result.stdout.strip()
 
-    rust_script_file=tempfile.NamedTemporaryFile(mode='w', dir='./lace')
-    rust_script_file.write(rust_script_contents)
-    rust_script_file.flush()
+    if len(code) == 0:
+        return 0
 
-    code_result=subprocess.run(['rust-script', rust_script_file.name])
-    exit(code_result.returncode)
-else:
-    exit(1)
+    if args.language == 'python':
+        enumerate_string_lines(code)
+        code_result=subprocess.run('python3', input=code, text=True)
+        exit(code_result.returncode)
+    elif args.language == 'rust':
+        rust_script_contents=f"""//! ```cargo
+    //! [dependencies]
+    //! lace = {{ path = ".", version="0.1.0" }}
+    //! ```
+    fn main() {{
+    {code}
+    }}
+    """
+
+        enumerate_string_lines(rust_script_contents)
+
+        rust_script_file=tempfile.NamedTemporaryFile(mode='w', dir='./lace')
+        rust_script_file.write(rust_script_contents)
+        rust_script_file.flush()
+
+        code_result=subprocess.run(['rust-script', rust_script_file.name])
+        return code_result.returncode
+    else:
+        return 1
+
+retval=process_file(args.file, args.language)
+exit(retval)
