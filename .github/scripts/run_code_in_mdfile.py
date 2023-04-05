493a760af925d0fd6d6432c1d8aa444c0d054165
Ken Swanson <ken.swanson@redpoll.ai>
2023-04-05 14:24:23 -0500

    Guard the filehandle

--- .github/scripts/run_code_in_mdfile.py
+++ .github/scripts/run_code_in_mdfile.py
@@ -18,39 +18,38 @@ def enumerate_string_lines(code):
     print("```")
 
 def process_file(file, language):
-    fh = open(file)
+    with open(file) as fh:
+        result=subprocess.run(['codedown', language + '*'], stdin=fh, capture_output=True, text=True)
 
-    result=subprocess.run(['codedown', language + '*'], stdin=fh, capture_output=True, text=True)
+        code = result.stdout.strip()
 
-    code = result.stdout.strip()
+        if len(code) == 0:
+            return 0
 
-    if len(code) == 0:
-        return 0
+        if args.language == 'python':
+            enumerate_string_lines(code)
+            code_result=subprocess.run('python3', input=code, text=True)
+            exit(code_result.returncode)
+        elif args.language == 'rust':
+            rust_script_contents=f"""//! ```cargo
+        //! [dependencies]
+        //! lace = {{ path = ".", version="0.1.0" }}
+        //! ```
+        fn main() {{
+        {code}
+        }}
+        """
 
-    if args.language == 'python':
-        enumerate_string_lines(code)
-        code_result=subprocess.run('python3', input=code, text=True)
-        exit(code_result.returncode)
-    elif args.language == 'rust':
-        rust_script_contents=f"""//! ```cargo
-    //! [dependencies]
-    //! lace = {{ path = ".", version="0.1.0" }}
-    //! ```
-    fn main() {{
-    {code}
-    }}
-    """
+            enumerate_string_lines(rust_script_contents)
 
-        enumerate_string_lines(rust_script_contents)
+            rust_script_file=tempfile.NamedTemporaryFile(mode='w', dir='./lace')
+            rust_script_file.write(rust_script_contents)
+            rust_script_file.flush()
 
-        rust_script_file=tempfile.NamedTemporaryFile(mode='w', dir='./lace')
-        rust_script_file.write(rust_script_contents)
-        rust_script_file.flush()
-
-        code_result=subprocess.run(['rust-script', rust_script_file.name])
-        return code_result.returncode
-    else:
-        return 1
+            code_result=subprocess.run(['rust-script', rust_script_file.name])
+            return code_result.returncode
+        else:
+            return 1
 
 retval=process_file(args.file, args.language)
 exit(retval)
