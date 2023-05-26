7addadb9a25e2445cc81ee3afa6fa0c4a3f36d9e
Baxter Eaves <bax@redpoll.ai>
2023-05-25 22:15:38 -0500

    docs and __repr__

--- pylace/src/metadata.rs
+++ pylace/src/metadata.rs
@@ -10,6 +10,18 @@ use pyo3::types::PyType;
 
 use crate::utils::to_pyerr;
 
+macro_rules! newtype_json_repr {
+    ($self: ident) => {{
+        serde_json::to_string_pretty(&$self.0).map_err(to_pyerr)
+    }};
+}
+
+macro_rules! newtype_string_repr {
+    ($self: ident) => {{
+        Ok($self.0.to_string())
+    }};
+}
+
 /// Column metadata
 #[pyclass]
 #[derive(Clone, Debug)]
@@ -34,13 +46,22 @@ impl ContinuousPrior {
     /// m: float
     ///     The prior mean
     /// k: float
+    ///     How strongly we believe the prior mean (in prior pseudo-
+    ///     observations)
     /// v: float
+    ///     How strongly we believe the prior variance (in prior pseudo-
+    ///     observations)
     /// s2: float
+    ///     The prior variance
     #[new]
     #[pyo3(signature = (m=0.0, k=1.0, v=1.0, s2=1.0))]
     pub fn new(m: f64, k: f64, v: f64, s2: f64) -> PyResult<Self> {
         let inner = NormalInvChiSquared::new(m, k, v, s2).map_err(to_pyerr)?;
         Ok(Self(inner))
+    }
+
+    pub fn __repr__(&self) -> PyResult<String> {
+        newtype_string_repr!(self)
     }
 }
 
@@ -76,28 +97,54 @@ impl ContinuousHyper {
             pr_s2: InvGamma::new(pr_s2.0, pr_s2.1).map_err(to_pyerr)?,
         }))
     }
+
+    pub fn __repr__(&self) -> PyResult<String> {
+        newtype_json_repr!(self)
+    }
 }
 
-#[derive(Clone, Debug)]
+/// Prior on categorical data
 #[pyclass]
+#[derive(Clone, Debug)]
 pub struct CategoricalPrior(pub SymmetricDirichlet);
 
-#[derive(Clone, Debug)]
+/// Hyperprior on categorical prior
 #[pyclass]
+#[derive(Clone, Debug)]
 pub struct CategoricalHyper(pub CsdHyper);
 
 #[pymethods]
 impl CategoricalPrior {
+    /// Create a new ``CategoricalPrior``
+    ///
+    /// Parameters
+    /// ----------
+    /// k: int
+    ///     The number of categories
+    /// alpha: float
+    ///     The symmetric Dirichlet weight
     #[new]
     #[pyo3(signature = (k, alpha=0.5))]
     pub fn new(k: usize, alpha: f64) -> PyResult<Self> {
         let inner = SymmetricDirichlet::new(alpha, k).map_err(to_pyerr)?;
         Ok(Self(inner))
     }
+
+    pub fn __repr__(&self) -> PyResult<String> {
+        newtype_string_repr!(self)
+    }
 }
 
 #[pymethods]
 impl CategoricalHyper {
+    /// Create a new ``CategoricalHyper``
+    ///
+    /// Parameters
+    /// ----------
+    /// shape: float
+    ///     The inverse gamma shape parameter
+    /// scale: float
+    ///     The inverse gamma scale parameter
     #[new]
     #[pyo3(signature = (shape=1.0, scale=1.0))]
     pub fn new(shape: f64, scale: f64) -> PyResult<Self> {
@@ -105,20 +152,37 @@ impl CategoricalHyper {
             pr_alpha: InvGamma::new(shape, scale).map_err(to_pyerr)?,
         }))
     }
+
+    pub fn __repr__(&self) -> PyResult<String> {
+        newtype_json_repr!(self)
+    }
 }
 
-#[derive(Clone, Debug)]
+/// A map of categorical values to unsigned integers
 #[pyclass]
+#[derive(Clone, Debug)]
 pub struct ValueMap(lace::codebook::ValueMap);
 
 #[pymethods]
 impl ValueMap {
+    /// Create a map of ``k`` unsigned integers
     #[classmethod]
-    #[pyo3(signature = (n_cats))]
-    pub fn int(_cls: &PyType, n_cats: usize) -> Self {
-        Self(lace::codebook::ValueMap::U8(n_cats))
+    #[pyo3(signature = (k))]
+    pub fn int(_cls: &PyType, k: usize) -> Self {
+        Self(lace::codebook::ValueMap::U8(k))
     }
 
+    /// Create a map from strings
+    ///
+    /// Parameters
+    /// ----------
+    /// values: list[str]
+    ///     A list of unique strings
+    ///
+    /// Raises
+    /// ------
+    /// ValueError
+    ///     The strings are not unique
     #[classmethod]
     #[pyo3(signature = (values))]
     pub fn string(_cls: &PyType, values: Vec<String>) -> PyResult<Self> {
@@ -127,14 +191,42 @@ impl ValueMap {
             .map(Self)
     }
 
+    /// Create a map from boolean
     #[classmethod]
     pub fn bool(_cls: &PyType) -> Self {
         Self(lace::codebook::ValueMap::Bool)
+    }
+
+    pub fn __repr__(&self) -> String {
+        use lace::codebook::ValueMap as Vm;
+        match self.0 {
+            Vm::U8(k) => format!("ValueMap (UInt, k={k})"),
+            Vm::String(ref inner) => {
+                let k = inner.len();
+                let cats = (0..k)
+                    .map(|ix| format!("'{}' ", inner.category(ix)))
+                    .collect::<String>();
+                format!("ValueMap (String) [ {cats}]")
+            }
+            Vm::Bool => String::from("ValueMap (bool)"),
+        }
     }
 }
 
 #[pymethods]
 impl ColumnMetadata {
+    /// Create continuous column metadata
+    ///
+    /// Parameters
+    /// ----------
+    /// name: str
+    ///     The name of the column
+    /// prior: ContinuousPrior, optional
+    ///     The prior on the data. If ``None`` (default), a prior will be drawn
+    ///     from the ``hyper``
+    /// hyper: ContinuousHyper, optional
+    ///     The prior on the data. If ``None`` (default) and ``prior`` is
+    ///     defined, the prior parameters will be locked.
     #[classmethod]
     #[pyo3(signature = (name, prior=None, hyper=None))]
     pub fn continuous(
@@ -154,6 +246,24 @@ impl ColumnMetadata {
         })
     }
 
+    /// Create categorical column metadata
+    ///
+    /// Parameters
+    /// ----------
+    /// name: str
+    ///     The name of the column
+    /// k: int
+    ///     The number of categories
+    /// value_map: ValueMap, optional
+    ///     A map from possible values to unsigned integers. If ``None``
+    ///     (default), it is assumed the values to this column take on values
+    ///     in [0, k-1].
+    /// prior: CategoricalPrior, optional
+    ///     The prior on the data. If ``None`` (default), a prior will be drawn
+    ///     from the ``hyper``
+    /// hyper: CategoricalHyper, optional
+    ///     The prior on the data. If ``None`` (default) and ``prior`` is
+    ///     defined, the prior parameters will be locked.
     #[classmethod]
     #[pyo3(signature = (name, k, value_map=None, prior=None, hyper=None))]
     pub fn categorical(
@@ -179,15 +289,21 @@ impl ColumnMetadata {
         })
     }
 
+    /// Set whether the column missing data should be modeled as non-random
     pub fn missing_not_at_random(&self, mnar: bool) -> Self {
         let mut out = self.clone();
         out.0.missing_not_at_random = mnar;
         out
     }
 
+    /// Add notes
     pub fn notes(&self, notes: Option<String>) -> Self {
         let mut out = self.clone();
         out.0.notes = notes;
         out
+    }
+
+    pub fn __repr__(&self) -> PyResult<String> {
+        newtype_json_repr!(self)
     }
 }
