35c3e439b31a7781e26010aecd2125c3b8e9687c
Mike Schmidt <schmidmt@gmail.com>
2023-09-01 08:52:07 -0600

    fix: Fixed a few pylace bugs
     - Fixed pylace's `__getitem__` index name to match other accessors.
     - Fixed pylace's `append_column_metadata` to refer to the correct internal function.
     - Pylace will try to use Python's internal conversion for floats in `value_to_datum` conversion.

--- pylace/src/utils.rs
+++ pylace/src/utils.rs
@@ -289,9 +289,6 @@ pub(crate) fn vec_to_srs(
             }
         }
         FType::Count => Ok(srs_from_vec!(values, name, u32, Count)),
-        // ftype => Err(PyErr::new::<PyValueError, _>(format!(
-        //     "Simulated unsupported ftype: {ftype:?}"
-        // ))),
     }
     .map(PySeries)
 }
@@ -358,9 +355,6 @@ pub(crate) fn simulate_to_df(
                 }
             }
             FType::Count => Ok(srs_from_simulate!(values, i, name, u32, Count)),
-            // ftype => Err(PyErr::new::<PyValueError, _>(format!(
-            //     "Simulated unsupported ftype: {ftype:?}"
-            // ))),
         }?;
         df.with_column(srs).map_err(|err| {
             PyErr::new::<PyRuntimeError, _>(format!(
@@ -537,10 +531,7 @@ pub(crate) fn value_to_datum(val: &PyAny, ftype: FType) -> PyResult<Datum> {
 
     match ftype {
         FType::Continuous => {
-            let x: f64 = {
-                let f: &PyFloat = val.downcast().unwrap();
-                f.value()
-            };
+            let x: f64 = val.extract().unwrap();
             if x.is_nan() {
                 Ok(Datum::Missing)
             } else {
