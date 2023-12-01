8b35406639c868d1cb0e50cd5312130e765f49fe
Baxter Eaves <bax@redpoll.ai>
2023-12-01 08:47:56 -0600

    Filling out state plot (partial progress)

--- pylace/lace/plot.py
+++ pylace/lace/plot.py
@@ -2,12 +2,12 @@
 
 from typing import Dict, Optional, Union
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 import polars as pl
-import matplotlib.pyplot as plt
 
 from lace import Engine
 
@@ -227,16 +227,64 @@ def _get_xs(engine, cat_rows, view_cols, compute_ps=False):
     else:
         return xs
 
+
+def _get_colors(engine: Engine, *, cmap: str = "gray_r", missing_color=None):
+    codebook = engine.codebook
+
+    if missing_color is None:
+        missing_color = (1.0, 0.0, 0.2, 1.0)
+
+    colors = {}
+    for col in engine.columns:
+        ftype = engine.ftype(col)
+        xs = engine[col][col]
+        if ftype == "Categorical":
+            valmap = codebook.value_map(col)
+            mapper = {v: k for k, v in enumerate(valmap.values())}
+            k = len(mapper) - 1
+
+            def _inner_norm(val):
+                return mapper[val] / k
+
+        else:
+            xmin = xs.min()
+            xmax = xs.max()
+
+            def _inner_norm(val):
+                return (val - xmin) / (xmax - xmin)
+
+        colormap = plt.cm.get_cmap(cmap)
+
+        def _norm(val):
+            if pd.isnull(val):
+                return missing_color
+            else:
+                return np.array(colormap(_inner_norm(val)))
+
+        colors[col] = [_norm(x) for x in xs]
+
+    df = pd.DataFrame(colors, index=engine.index)
+    return df[engine.columns]
+
+
 def state(
     engine: Engine,
     state_ix: int,
-    missing_color = None,
-    cat_gap:int = 1,
-    view_gap:int = 1,
-    ax = None,
+    *,
+    cmap: Optional[str] = None,
+    missing_color=None,
+    cat_gap: int = 1,
+    view_gap: int = 1,
+    show_index: bool = True,
+    show_columns: bool = True,
+    aspect=None,
+    ax=None,
 ):
     if ax is None:
         ax = plt.gca()
+
+    if cmap is None:
+        cmap = "gray_r"
 
     n_rows, n_cols = engine.shape
     col_asgn = engine.column_assignment(state_ix)
@@ -248,15 +296,21 @@ def state(
     dim_col = n_cols + (n_views - 1) * view_gap
     dim_row = n_rows + (max_cats - 1) * cat_gap
 
-    zs = np.zeros((dim_row, dim_col)) + 0.1
+    zs = np.zeros((dim_row, dim_col, 4))
 
     row_names = engine.index
-    col_names = engine.columns 
+    col_names = engine.columns
+
+    view_counts = np.bincount(col_asgn)
+    view_ixs = np.argsort(view_counts)[::-1]
+
+    colors = _get_colors(engine, cmap=cmap, missing_color=missing_color)
 
     col_start = 0
-    for view_ix, row_asgn in enumerate(row_asgns):
+    for view_ix in view_ixs:
+        row_asgn = row_asgns[view_ix]
         row_start = 0
-        n_cats = max(row_asgn) + 1
+        max(row_asgn) + 1
         view_len = sum(z == view_ix for z in col_asgn)
         view_cols = [i for i, z in enumerate(col_asgn) if z == view_ix]
 
@@ -267,40 +321,86 @@ def state(
         ixs = np.argsort(ps)[::-1]
         view_cols = [view_cols[ix] for ix in ixs]
 
-        for i, col in enumerate(view_cols):
-            ax.text(col_start + i, -1, col_names[col], ha='center', va='bottom', rotation=90)
+        if show_columns:
+            for i, col in enumerate(view_cols):
+                ax.text(
+                    col_start + i,
+                    -1,
+                    col_names[col],
+                    ha="center",
+                    va="bottom",
+                    rotation=90,
+                )
 
-        for cat_ix in range(n_cats):
+        cat_counts = np.bincount(row_asgn)
+        cat_ixs = np.argsort(cat_counts)[::-1]
+
+        for cat_ix in cat_ixs:
             cat_len = sum(z == cat_ix for z in row_asgn)
             cat_rows = [i for i, z in enumerate(row_asgn) if z == cat_ix]
 
             xs, ps = _get_xs(engine, cat_rows, view_cols, compute_ps=True)
             ixs = np.argsort(ps)[::-1]
             cat_rows = [cat_rows[ix] for ix in ixs]
-            xs = _get_xs(engine, cat_rows, view_cols)
-            zs[row_start:row_start+cat_len, col_start:col_start + view_len] = xs
+
+            cs = colors.iloc[cat_rows, view_cols].values.tolist()
+            cs = np.asarray(cs, dtype=float)
+            zs[
+                row_start : row_start + cat_len,
+                col_start : col_start + view_len,
+            ] = cs
 
             # label rows
-            for i, row in enumerate(cat_rows):
-                ax.text(col_start-1, i + row_start, row_names[row], ha='right', va='center')
+            if show_index:
+                for i, row in enumerate(cat_rows):
+                    ax.text(
+                        col_start - 1,
+                        i + row_start,
+                        row_names[row],
+                        ha="right",
+                        va="center",
+                    )
+
+            ax.text(
+                col_start + view_counts[view_ix] / 2,
+                row_start + cat_counts[cat_ix],
+                f"$C_{{{cat_ix}}}$",
+                ha="center",
+                va="top",
+            )
 
             row_start += cat_len + cat_gap
 
+        ax.text(
+            col_start + view_counts[view_ix] / 2,
+            dim_row + cat_gap,
+            f"$V_{{{view_ix}}}$",
+            ha="center",
+            va="top",
+        )
         col_start += view_len + view_gap
 
-
-    ax.matshow(zs, cmap='gray_r')
-
+    ax.matshow(zs, cmap="gray_r", aspect=aspect)
 
 
 if __name__ == "__main__":
     from lace.examples import Animals
 
     eng = Animals()
-    plt.figure(tight_layout=True, facecolor='#e8e8e8')
+    # eng = Satellites()
+    plt.figure(tight_layout=True, facecolor="Gainsboro")
     ax = plt.gca()
-    state(eng, 1, view_gap=15, ax=ax)
-    plt.axis('off')
+    state(
+        eng,
+        1,
+        view_gap=15,
+        cat_gap=2,
+        ax=ax,
+        show_index=True,
+        show_columns=True,
+        cmap="cubehelix",
+    )
+    plt.axis("off")
     plt.show()
     # import doctest
 
