bfa84161d4f0a8405e78bf5a629a1016ffb59c7b
Baxter Eaves <bax@redpoll.ai>
2024-01-24 21:31:44 -0600

    do not test plot module if NOPLOT

--- pylace/tests/test_docs.py
+++ pylace/tests/test_docs.py
@@ -5,12 +5,11 @@ from importlib import import_module
 import polars as pl
 from plotly import io
 
-HIDE_PLOT = os.environ.get("LACE_DOCTEST_NOPLOT", "0") == "1"
+NOPLOT = os.environ.get("LACE_DOCTEST_NOPLOT", "0") == "1"
 
 
-if HIDE_PLOT:
-    # io.renderers.default = 'json'
-    io.renderers.default = "iframe"
+if NOPLOT:
+    io.renderers.default = "json"
 
 
 pl.Config(tbl_rows=8)
@@ -24,28 +23,15 @@ class _Context(dict):
         return self
 
 
-def plot():
-    module = import_module("lace.plot")
+def runtest(mod):
+    module = import_module(mod)
     extraglobs = _Context(module.__dict__.copy())
-
-    doctest.testmod(module, extraglobs=extraglobs)
-
-
-def engine():
-    module = import_module("lace.engine")
-    extraglobs = _Context(module.__dict__.copy())
-
-    doctest.testmod(module, extraglobs=extraglobs)
-
-
-def analysis():
-    module = import_module("lace.analysis")
-    extraglobs = _Context(module.__dict__.copy())
-
     doctest.testmod(module, extraglobs=extraglobs)
 
 
 if __name__ == "__main__":
-    engine()
-    analysis()
-    plot()
+    runtest("lace.engine")
+    runtest("lace.analysis")
+
+    if not NOPLOT:
+        runtest("lace.plot")
