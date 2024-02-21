03c4a196a3baa2c4950f8224cc3dd61a26088fec
Ken Swanson <ken.swanson@redpoll.ai>
2024-02-20 19:38:45 -0600

    Set `__version__` from the package metadata

--- pylace/lace/__init__.py
+++ pylace/lace/__init__.py
@@ -1,5 +1,6 @@
 """The Python bindings for the Lace ML tool."""
 
+from importlib import metadata
 from lace import core
 from lace.codebook import Codebook
 from lace.core import (
@@ -36,4 +37,4 @@ __all__ = [
     "ValueMap",
 ]
 
-__version__ = "0.6.0"
+__version__ = metadata.version('pylace')
