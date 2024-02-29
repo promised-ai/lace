87fb1fe315d4de3354b5faaec25f64d282d94d2f
Ken Swanson <ken.swanson@redpoll.ai>
2024-02-29 16:44:20 -0600

    Added python test to ensure that boolean dataframe columns get typed correctly

--- pylace/tests/test_codebook.py
+++ pylace/tests/test_codebook.py
@@ -39,3 +39,24 @@ def test_engine_from_pandas_with_codebook_smoke():
     engine = lace.Engine.from_df(df, codebook=codebook, n_states=3)
     assert engine.shape == (14, 2)
     assert engine.columns == ["x", "y"]
+
+def test_engine_with_boolean_string_columns():
+    n = 14
+    df = pl.DataFrame(
+        {
+            "ID": list(range(n)),
+            "x": np.random.randn(n),
+            "b": np.random.choice([True, False], size=n)
+        }
+    )
+
+    assert df.dtypes[df.get_column_index("b")] == pl.Boolean
+
+    codebook = lace.Codebook.from_df("test", df)
+    assert codebook.shape == (n, 2)
+    assert str(codebook.column_metadata["b"].value_map) == str(lace.ValueMap.bool())
+
+    engine = lace.Engine.from_df(df, codebook=codebook, n_states=3)
+    assert engine.shape == (n, 2)
+    assert engine.columns == ["x", "b"]
+    assert str(engine.codebook.column_metadata["b"].value_map) == str(lace.ValueMap.bool())
